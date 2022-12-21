44d4e310b09cfee38454e2c33b83b96ca50d8730
Phu <94579297+PhuTNPH17442@users.noreply.github.com>
2022-12-22 01:51:20 +0700

    gitignore

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const firebase = require('firebase')
 const handlebars = require('express-handlebars')
 const path = require('path')
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 app.use(express.urlencoded({
   extended : true
 }));
